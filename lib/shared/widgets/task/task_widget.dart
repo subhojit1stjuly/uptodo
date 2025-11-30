b4de07ab34bdc9fc1506d2e59f104106e45e2a14
Subhojit <subhojit1stjuly@gmail.com>
2025-11-30 22:53:49 +0530

    Refactor TaskWidget to use ListTile and enhance layout with title, subtitle, and category/priority widgets

--- lib/shared/widgets/task/task_widget.dart
+++ lib/shared/widgets/task/task_widget.dart
@@ -3,9 +3,10 @@ import 'package:go_router/go_router.dart';
 import 'package:uptodo/core/di/injector.dart';
 import 'package:uptodo/core/routing/route_constants.dart';
 import 'package:uptodo/core/utils/common_extainsions.dart';
+import 'package:uptodo/features/category/presentation/widget/categor_widget.dart';
 import 'package:uptodo/features/task_details/data/model/task_model/task_model.dart';
-import 'package:uptodo/features/task_details/presentation/widget/task_card.dart';
 import 'package:uptodo/shared/model/shred_enums.dart';
+import 'package:uptodo/shared/widgets/task/priority_widget.dart';
 
 /// TaskCard
 class TaskWidget extends StatelessWidget {
@@ -28,19 +29,62 @@ class TaskWidget extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return InkWell(
+    return ListTile(
+      leading: Checkbox(value: false, onChanged: (value) {}),
       onTap: () {
         getIt<GoRouter>().pushNamed(
           RouteConstants.taskDetails,
           extra: (taskModel, dayFilterType.value(context)),
         ).then((_) => onEditCompleted());
       },
-      child: TaskListTileCard(
-        taskTitle: taskModel.title,
-        taskTime: taskModel.taskTime.convertTimeOfDayToString(),
-        taskCategory: taskModel.category,
-        priority: taskModel.priorityId,
-        timeOfTheWeek: dayFilterType.value(context),
+      title: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        spacing: 8,
+        children: [
+          Text(
+            taskModel.title,
+            style: Theme
+                .of(context)
+                .textTheme
+                .bodyLarge
+                ?.copyWith(
+              fontWeight: FontWeight.w600,
+            ),
+          ),
+          const SizedBox.shrink(),
+        ],
+      ),
+      subtitle: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceAround,
+        children: [
+          Text(
+            '${dayFilterType.value(context)} At ${taskModel.taskTime
+                .convertTimeOfDayToString()}',
+            style: Theme
+                .of(context)
+                .textTheme
+                .bodySmall
+                ?.copyWith(
+              fontWeight: FontWeight.w400,
+            ),
+          ),
+          Expanded(
+            child: Row(
+              mainAxisSize: MainAxisSize.min,
+              mainAxisAlignment: MainAxisAlignment.end,
+              spacing: 8,
+              children: <Widget>[
+                CategoryWidget(
+                  taskCategory: taskModel.category,
+                ),
+                PriorityWidget(
+                  priority: taskModel.priorityId.toString(),
+                ),
+                const SizedBox(width: 4),
+              ],
+            ),
+          ),
+        ],
       ),
     );
   }
